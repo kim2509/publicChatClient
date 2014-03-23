2be4511133744aed88e9680a4fbe76b0039bac84
Daeyong Kim <kim2509@gmail.com>
2014-03-23 22:04:52 +0900

    add homeliest item

---
+++ publicChat/src/com/dy/domain/HomeListItem.java
@@ -0,0 +1,14 @@
+package com.dy.domain;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+import android.view.View;
+
+public class HomeListItem {
+
+	@JsonIgnore
+	public void setViewData( View view )
+	{
+		
+	}
+}
